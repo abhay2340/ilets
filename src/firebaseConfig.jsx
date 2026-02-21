358d077b8bfd0e8dc5d78b67a3557ef30f7504c3
Saurabh-BigOh <saurabh-bigoh@Saurabh-BigOhs-MacBook-Air.local>
2026-02-21 17:48:38 +0530

    fix: firebase config from env

--- src/firebaseConfig.jsx
+++ src/firebaseConfig.jsx
@@ -8,13 +8,13 @@ import { getStorage } from 'firebase/storage';
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCEbJIGgj2820WJD_N7CTZVs7avYIMq0DA",
-  authDomain: "real-ielts-248af.firebaseapp.com",
-  projectId: "real-ielts-248af",
-  storageBucket: "real-ielts-248af.firebasestorage.app",
-  messagingSenderId: "1039608226269",
-  appId: "1:1039608226269:web:98ae2dd5bc1f0f0a959634",
-  measurementId: "G-D901HQ44C6"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
